813813fa75481fdc5cf60a8079c057ebbf53e64e
Ivan <shelkovnikov_i.a@mail.ru>
2020-02-21 19:48:40 +0500

    Updates

---
+++ precache-manifest.cbe249acbe1f129b895ff40e43dcae03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6023e817bfeef6116f56162e0551ad95",
+    "url": "/demo-react/index.html"
+  },
+  {
+    "revision": "0fe9ccc1c28a1340d35a",
+    "url": "/demo-react/static/css/main.27d16dac.chunk.css"
+  },
+  {
+    "revision": "a33b0e99383aba71bcf4",
+    "url": "/demo-react/static/js/2.43681b6b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/demo-react/static/js/2.43681b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe9ccc1c28a1340d35a",
+    "url": "/demo-react/static/js/main.6bc6ba51.chunk.js"
+  },
+  {
+    "revision": "fb713fae21d1c1c2e898",
+    "url": "/demo-react/static/js/runtime-main.6b61c35f.js"
+  },
+  {
+    "revision": "19383d38b404d3cbd17d6aae7366dd03",
+    "url": "/demo-react/static/media/Summoners_Rift.19383d38.jpg"
+  },
+  {
+    "revision": "12d9c7c2255431253550091cec087212",
+    "url": "/demo-react/static/media/background.12d9c7c2.jpg"
+  }
+]);
